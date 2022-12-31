56daa0266610923fa728e9b95c06852e9638100f
Emilio Nicoletti <nico@pop-os.localdomain>
2022-12-30 21:22:17 -0300

    create the landing screen

---
+++ lib/screens/onboding/onboding_screen.dart
@@ -0,0 +1,95 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+import 'components/animated_btn.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation("active", autoplay: false);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              width: MediaQuery.of(context).size.width * 1.7,
+              bottom: 200,
+              left: 100,
+              child: Image.asset("assets/Backgrounds/Spline.png")),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+            ),
+          ),
+          const RiveAnimation.asset(
+            "assets/RiveAssets/shapes.riv",
+            fit: BoxFit.cover,
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  SizedBox(
+                    width: 260,
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Sua aprovação na palma da sua mão",
+                          style: TextStyle(
+                            fontSize: 45,
+                            fontFamily: "Poppins",
+                            height: 1.2,
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        Text(
+                          '''Resolva milhares de questões comentadas, veja seu desempenho, receba lembretes e interaja com outros 'smarts' como você''',
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                  AnimatedBtn(
+                    btnAnimationController: _btnAnimationController,
+                    press: () {
+                      _btnAnimationController.isActive = true;
+                    },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                        "Seja Smart Premium e tenha acesso a +20 recursos exclusivos para impulsionar sua aprovação."),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
