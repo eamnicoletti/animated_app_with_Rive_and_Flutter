221d85671a5de96dd8a9d1de0bdaccdd726a05c9
Emílio Nicoletti <eamnicoletti@gmail.com>
2023-02-10 23:00:14 -0300

    add 3D rotation in side menu

--- lib/entry_point.dart
+++ lib/entry_point.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:rive_animation/components/side_menu.dart';
@@ -71,13 +73,19 @@ class _EntryPointState extends State<EntryPoint>
             height: MediaQuery.of(context).size.height,
             child: const SideMenu(),
           ),
-          Transform.translate(
-            offset: Offset(animation.value * 288, 0),
-            child: Transform.scale(
-              scale: scalAnimation.value,
-              child: const ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(24)),
-                child: HomeScreen(),
+          Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(animation.value - 30 * animation.value * pi / 180),
+            child: Transform.translate(
+              offset: Offset(animation.value * 265, 0),
+              child: Transform.scale(
+                scale: scalAnimation.value,
+                child: const ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(24)),
+                  child: HomeScreen(),
+                ),
               ),
             ),
           ),
